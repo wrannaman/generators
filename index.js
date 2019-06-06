b358a6f98f03aea48f7b52a328b5c811d4d0f56e
Wrannaman <andrewpierno@gmail.com>
2019-06-06 12:22:50 -0700

    dont gen old graphql

--- index.js
+++ index.js
@@ -133,9 +133,9 @@ const letzGetIt = async () => {
   await writeDockerIgnore(args);
   await readme(args);
   await makeTests(args);
-  if (args.flavor === 'graphql') {
-    await makeGraphql(args);
-  }
+  // if (args.flavor === 'graphql') {
+  //   await makeGraphql(args);
+  // }
   if (args.logging) console.log('all done 🚀'); // eslint-disable-line
 };
 
